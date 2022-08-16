01338c08f0bc330f2fd3b21fb376df12c46d4c4e
yamada <shinya.yamada@benext-corp.com>
2022-08-17 08:06:07 +0900

    ⚡️ update tailwindcss settings

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,15 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [
-    "./pages/**/*.{js,ts,jsx,tsx}",
-    "./components/**/*.{js,ts,jsx,tsx}",
-  ],
+  mode: "jit",
+  corePlugins: {
+    preflight: false,
+  },
+  content: ["./src/**/*.{js,jsx,ts,tsx}"],
+  darkMode: false,
   theme: {
+    extend: {},
+  },
+  variants: {
     extend: {},
   },
   plugins: [],
