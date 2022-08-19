02cabdf447e302c78775d25e3cfd311485c3cdb2
yamada <shinya.yamada@benext-corp.com>
2022-08-19 09:53:29 +0900

    ⚡️ update header link

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,50 +1,53 @@
+import Link from "next/link";
 import React from "react";
 
 export const Header = () => {
   return (
     <header>
-      <nav className="px-4 lg:px-6 py-2.5">
+      <nav className="py-2.5 mx-20">
         <div className="flex flex-wrap justify-between">
-          <a href="#" className="flex items-center no-underline">
-            <span className="font-bold text-gray-700">Yama IT Portfolio</span>
-          </a>
+          <Link href="/" className="flex items-center no-underline">
+            <span className="font-bold text-gray-700 hover:cursor-pointer">
+              Yama IT Portfolio
+            </span>
+          </Link>
           <div
             className=" justify-between items-center flex w-auto"
             id="mobile-menu-2"
           >
             <ul className="flex mt-4 flex-row lg:space-x-8 lg:mt-0 list-none font-bold">
               <li>
-                <a
-                  href="#"
+                <Link
+                  href="/About"
                   className="block py-2 pr-4 pl-3 text-gray-700 no-underline rounded bg-primary-700 lg:p-0 dark:text-white"
                   aria-current="page"
                 >
                   About
-                </a>
+                </Link>
               </li>
               <li>
-                <a
-                  href="#"
+                <Link
+                  href="/Blog"
                   className="block py-2 pr-4 pl-3 text-gray-700 no-underline rounded bg-primary-700 lg:p-0 dark:text-white"
                 >
                   Blog
-                </a>
+                </Link>
               </li>
               <li>
-                <a
-                  href="#"
+                <Link
+                  href="/Portfolio"
                   className="block py-2 pr-4 pl-3 text-gray-700 no-underline rounded bg-primary-700 lg:p-0 dark:text-white"
                 >
                   Portfolio
-                </a>
+                </Link>
               </li>
               <li>
-                <a
-                  href="#"
+                <Link
+                  href="/Contact"
                   className="block py-2 pr-4 pl-3 text-gray-700 no-underline rounded bg-primary-700 lg:p-0 dark:text-white"
                 >
                   Contact
-                </a>
+                </Link>
               </li>
             </ul>
           </div>
