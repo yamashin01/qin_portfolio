b4fa6149f83435383db0f5dbea3872369ed171ab
yamada <shinya.yamada@benext-corp.com>
2022-08-24 07:30:49 +0900

    ✨ get blog data from microCMS by SSG

---
+++ src/pages/Blog/[id].tsx
@@ -0,0 +1,44 @@
+import { MicroCMSContentId, MicroCMSDate } from "microcms-js-sdk";
+import { GetStaticPaths, GetStaticProps, NextPage } from "next";
+import { client } from "src/libs/client";
+import { Blog } from ".";
+
+type Props = Blog & MicroCMSContentId & MicroCMSDate;
+
+const BlogId: NextPage<Props> = (props) => {
+  console.log(props);
+  return (
+    <div>
+      <h1>{props.title}</h1>
+      <time>{props.publishedAt}</time>
+      <div dangerouslySetInnerHTML={{ __html: props.body }} />
+    </div>
+  );
+};
+
+export const getStaticPaths: GetStaticPaths<{ id: string }> = async () => {
+  const data = await client.getList({ endpoint: "blog" });
+  const ids = data.contents.map((content) => `/Blog/${content.id}`);
+  return {
+    paths: ids,
+    fallback: false,
+  };
+};
+
+export const getStaticProps: GetStaticProps<Props, { id: string }> = async (
+  ctx
+) => {
+  if (!ctx.params) {
+    return { notFound: true };
+  }
+  const data = await client.getListDetail<Blog>({
+    endpoint: "blog",
+    contentId: ctx.params.id,
+  });
+  console.log(data);
+
+  return {
+    props: data,
+  };
+};
+export default BlogId;
---
+++ src/pages/Blog/index.tsx
@@ -0,0 +1,41 @@
+import { Title } from "@mantine/core";
+import { MicroCMSListResponse } from "microcms-js-sdk";
+import { GetStaticProps, NextPage } from "next";
+import Link from "next/link";
+import { client } from "src/libs/client";
+
+export type Blog = {
+  title: string;
+  body: string;
+};
+
+type Props = MicroCMSListResponse<Blog>;
+
+export const getStaticProps: GetStaticProps<Props> = async () => {
+  const data = await client.getList<Blog>({ endpoint: "blog" });
+  return {
+    props: data,
+  };
+};
+
+const Blog: NextPage<Props> = (props) => {
+  return (
+    <>
+      <div className="border border-gray-200 py-4">
+        <Title>Blog</Title>
+      </div>
+      <p>{props.totalCount}件の記事</p>
+      {props.contents.map((content) => {
+        return (
+          <div key={content.id} className="mb-4">
+            <Link href={`/Blog/${content.id}`}>
+              <a>{content.title}</a>
+            </Link>
+          </div>
+        );
+      })}
+    </>
+  );
+};
+
+export default Blog;
