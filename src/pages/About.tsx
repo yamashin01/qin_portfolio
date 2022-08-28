3c143edb2092a0d8ccf997b7ba65afab4639bb0c
yamada <shinya.yamada@benext-corp.com>
2022-08-28 16:47:33 +0900

    ✨ conbine about page with microCMS

--- src/pages/About.tsx
+++ src/pages/About.tsx
@@ -1,17 +1,33 @@
 import { Title } from "@mantine/core";
+import { MicroCMSListResponse } from "microcms-js-sdk";
+import { GetStaticProps, NextPage } from "next";
+import { client } from "src/libs/client";
+import { AboutType } from "src/types/types";
 
-const About = () => {
+type Props = MicroCMSListResponse<AboutType>;
+
+const About:NextPage<Props> = (props) => {
   return (
     <>
       <div className="border border-gray-200 py-4">
         <Title>About</Title>
       </div>
       <div>
-        <h3>Ligtsound Yama</h3>
-        <div>ITエンジニア。</div>
+        <div className="mb-4">
+          <Title order={1}>{props.contents[0].name}</Title>
+        </div>
+        <div>{props.contents[0].profile}</div>
       </div>
     </>
   );
 };
+
+export const getStaticProps: GetStaticProps<Props> = async () => {
+  const data = await client.getList<AboutType>({ endpoint: "about" });
+  return {
+    props: data,
+  };
+};
+
 
 export default About;
