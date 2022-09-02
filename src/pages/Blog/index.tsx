7f08d7b4d083b18c89749b04c0830453c7750939
yamada <shinya.yamada@benext-corp.com>
2022-09-02 12:53:45 +0900

    ⚡️ modify layout of blog index page

--- src/pages/Blog/index.tsx
+++ src/pages/Blog/index.tsx
@@ -4,6 +4,7 @@ import { GetStaticProps, NextPage } from "next";
 import Link from "next/link";
 import { client } from "src/libs/client";
 import { BlogType } from "src/types/types";
+import { format } from "date-fns";
 
 type Props = MicroCMSListResponse<BlogType>;
 
@@ -20,13 +21,20 @@ const Blog: NextPage<Props> = (props) => {
       <div className="border border-gray-200 py-4">
         <Title>Blog</Title>
       </div>
-      <p>{props.totalCount}件の記事</p>
       {props.contents.map((content) => {
         return (
           <div key={content.id} className="mb-4">
-            <Link href={`/Blog/${content.id}`}>
-              <a>{content.title}</a>
-            </Link>
+            <div className="no-underline text-blue-500 hover:text-blue-800 hover:cursor-pointer text-xl">
+              <Link href={`/Blog/${content.id}`}>
+                <Text>{content.title}</Text>
+              </Link>
+            </div>
+            <Text lineClamp={2}>
+                  <div dangerouslySetInnerHTML={{ __html: content.body}} />
+                </Text>
+                <div className="text-gray-500">
+                  <small>{format(new Date(content.publishedAt), "yyyy.MM.dd")}</small>
+                </div>
           </div>
         );
       })}
