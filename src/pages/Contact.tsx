dd84145d2f99b26e5cdb21e8133f3d2a6964dada
yamada <shinya.yamada@benext-corp.com>
2022-09-26 11:17:55 +0900

    ✨ set mailing system when sending contact form

--- src/pages/Contact.tsx
+++ src/pages/Contact.tsx
@@ -1,13 +1,38 @@
 import { Button, Textarea, TextInput, Title, useMantineTheme } from "@mantine/core";
-import { useState } from "react";
 import React from "react";
+import { useForm } from "@mantine/form";
+import axios from "axios";
 
 const Contact = () => {
-  const [email, setEmail] = useState("");
-  const [name, setName] = useState("");
-  const [message, setMessage] = useState("");
 
   const theme = useMantineTheme();
+
+  const form = useForm({
+    initialValues: {
+      email: "",
+      name: "",
+      message: "",
+    },
+
+    validate: {
+      email: (value) => (/^\S+@\S+$/.test(value) ? null : 'Invalid email'),
+    },
+  });
+
+  const handleSubmit = async(values: any) => {
+    try {
+      if (!process.env.NEXT_PUBLIC_MICROCMS_API_KEY) return null;
+      await axios.post("https://zx1fjxug3n.microcms.io/api/v1/contact", values, {
+        headers: {
+          "Content-Type": "application/json",
+          "X-MICROCMS-API-KEY": process.env.NEXT_PUBLIC_MICROCMS_API_KEY,
+        }          
+      })
+      form.reset();
+    } catch {
+      alert('API Error POST');
+    }
+  }
 
   return (
     <div>
@@ -15,31 +40,36 @@ const Contact = () => {
         <Title>Contact</Title>
       </div>
       <div className="bg-gray-100 h-px rounded-full mb-8" />
-      <div className="gap-2">
-        <TextInput
-          className="w-full py-4"
-          label="Email"
-          placeholder="your@email.com"
-          onChange={(e) => setEmail(e.currentTarget.value)}
-        />
-        <TextInput
-          className="w-full py-4"
-          label="Name"
-          placeholder="Taro Yamada"
-          onChange={(e) => setName(e.currentTarget.value)}
-        />
-        <Textarea
-          className="w-full py-4"
-          label="Your Message"
-          placeholder="I want to order your goods"
-          onChange={(e) => setMessage(e.currentTarget.value)}
-        />
-      </div>
-      <div className="text-center py-4">
-        <Button color={theme.colorScheme === 'dark' ? "gray" : "dark"} radius="xl">
-          Send Message
-        </Button>
-      </div>
+      <form onSubmit={form.onSubmit(handleSubmit)}>
+        <div className="gap-2">
+          <TextInput
+            className="w-full py-4"
+            label="Email"
+            placeholder="your@email.com"
+            required
+            {...form.getInputProps("email")}
+          />
+          <TextInput
+            className="w-full py-4"
+            label="Name"
+            placeholder="Taro Yamada"
+            required
+            {...form.getInputProps("name")}
+          />
+          <Textarea
+            className="w-full py-4"
+            label="Your Message"
+            placeholder="アプリ開発について相談したい。"
+            required
+            {...form.getInputProps("message")}
+          />
+        </div>
+        <div className="text-center py-4">
+          <Button color={theme.colorScheme === 'dark' ? "gray" : "dark"} radius="xl" type="submit">
+            Send Message
+          </Button>
+        </div>
+      </form>
     </div>
   );
 };
